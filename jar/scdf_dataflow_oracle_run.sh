0bf1df5a9904026247d8a16791795aa27b8246cb
marksman703 <marksman703@hanwha.com>
2024-10-22 23:01:26 +0900

    삽질의 기록 업데이트

---
+++ jar/scdf_dataflow_oracle_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# source
+source ../config.env
+
+# JDBC 드라이버 포함하여 SCDF 실행
+java -jar "spring-cloud-dataflow-server-2.11.5.jar" \
+--spring.datasource.url=jdbc:oracle:thin:@//$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME \
+--spring.datasource.username=$DATABASE_USERNAME \
+--spring.datasource.password=$DATABASE_PASSWORD \
+--spring.datasource.driver-class-name=$DATABASE_DRIVER_CLASS_NAME \
+--spring.cloud.dataflow.features.streams-enabled=false \
+--spring.flyway.enabled=false
+
+exit 0
+
