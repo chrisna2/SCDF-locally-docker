b699bc04c41965843c3ec98cf890fc68d93af634
marksman703 <marksman703@hanwha.com>
2024-10-26 13:23:32 +0900

    jar 생성 및 빌드처리

--- jar/scdf_dataflow_oracle_run.sh
+++ jar/scdf_dataflow_oracle_run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # 환경 변수 입력
-source ../config.env
+DATABASE_HOST=localhost
+DATABASE_PORT=1521
+DATABASE_NAME=xe
+DATABASE_USERNAME=HBT
+DATABASE_PASSWORD=password
+DATABASE_DRIVER_CLASS_NAME=oracle.jdbc.OracleDriver
+
 
 # JDBC 드라이버 포함하여 SCDF 실행
-java -jar "spring-cloud-dataflow-server-2.11.5.jar" \
+java -jar "spring-cloud-dataflow-server-2.11.0.jar" \
 --spring.datasource.url=jdbc:oracle:thin:@//$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME \
 --spring.datasource.username=$DATABASE_USERNAME \
 --spring.datasource.password=$DATABASE_PASSWORD \
